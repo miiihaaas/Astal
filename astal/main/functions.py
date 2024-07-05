b80fe9c62bfe1f7a961a02ba984b03a4c9b54180
miiihaaas <miiihaaas@gmail.com>
2024-07-05 20:48:20 +0200

    izbrisao evro gde ne treba

--- astal/main/functions.py
+++ astal/main/functions.py
@@ -313,7 +313,7 @@ def send_email(user, new_reservation, language):
     <li><strong>Vreme:</strong> {new_reservation.start_time}</li>
     <li><strong>Ime gosta:</strong> {user.name}</li>
     <li><strong>Broj osoba:</strong> {new_reservation.number_of_people}</li>
-    <li><strong>Napomena:</strong> {new_reservation.note} €</li>
+    <li><strong>Napomena:</strong> {new_reservation.note}</li>
 </ul>'''
 
         if new_reservation.amount < 0: #! ispravi ovo u '>0' kad se implementira plaćanje
@@ -343,7 +343,7 @@ def send_email(user, new_reservation, language):
     <li><strong>Time:</strong> {new_reservation.start_time}</li>
     <li><strong>Guest name:</strong> {user.name}</li>
     <li><strong>Number of people:</strong> {new_reservation.number_of_people}</li>
-    <li><strong>Note:</strong> {new_reservation.note} €</li>
+    <li><strong>Note:</strong> {new_reservation.note}</li>
 </ul>'''
 
         if new_reservation.amount < 0: #! correct this to '>0' when payment is implemented
