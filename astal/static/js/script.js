91d20d55d666eff469a33129de852dc5a0809494
MiodragStojkov <miodragstojkovnb@gmail.com>
2026-03-17 09:37:22 +0100

    Miki-17-03-2026-StPatrick

--- astal/static/js/script.js
+++ astal/static/js/script.js
@@ -3,22 +3,20 @@ let ballX = 0, ballY = 0;
 
 const ball = document.getElementById('ball');
 
-// Funkcija za postavljanje kuglice na početnu poziciju
 function setInitialPosition(event) {
-    mouseX = event.clientX;
-    mouseY = event.clientY;
+    mouseX = event.clientX + window.scrollX;
+    mouseY = event.clientY + window.scrollY;
     ballX = mouseX;
     ballY = mouseY;
     ball.style.transform = `translate(${ballX - ball.clientWidth / 2}px, ${ballY - ball.clientHeight / 2}px)`;
-    document.removeEventListener('mousemove', setInitialPosition); // Uklanja event listener nakon postavljanja početne pozicije
+    document.removeEventListener('mousemove', setInitialPosition);
 }
 
-// Postavlja početnu poziciju kuglice kada se prvi put pomeri kursor
 document.addEventListener('mousemove', setInitialPosition);
 
 document.addEventListener('mousemove', (event) => {
-    mouseX = event.clientX;
-    mouseY = event.clientY;
+    mouseX = event.clientX + window.scrollX;
+    mouseY = event.clientY + window.scrollY;
 });
 
 function animate() {
@@ -32,19 +30,18 @@ function animate() {
 
 animate();
 
-// Dodavanje event listenera za aktivne elemente
 const activeElements = document.querySelectorAll('input, a, button, select, textarea');
 
 activeElements.forEach(element => {
     element.addEventListener('mouseover', () => {
         ball.style.width = '120px';
         ball.style.height = '120px';
-        ball.style.opacity = '0.2'; // Dodajemo smanjenje opacity-a
+        ball.style.opacity = '0.2';
     });
 
     element.addEventListener('mouseout', () => {
         ball.style.width = '12px';
         ball.style.height = '12px';
-        ball.style.opacity = '1'; // Vraćamo opacity na 1
+        ball.style.opacity = '1';
     });
 });
